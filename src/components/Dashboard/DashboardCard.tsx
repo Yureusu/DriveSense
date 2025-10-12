7928f635a4a37f46ae6d790273357f93400a2149
unknown <yureusu09@gmail.com>
2025-10-12 06:38:25 -0700

    Fixed some bugs

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -8,7 +8,8 @@ type DashboardCardProps = {
 function DashboardCard({title, descrip ,isDark}: DashboardCardProps) {
 
     return (
-        <div className={`text-[var(--light-color)] bg-[var(--purple-color)]
+        <div className={`${isDark? "" : ""}
+            text-[var(--light-color)] bg-[var(--purple-color)]
             h-auto w-full flex flex-col items-start justify-center rounded-lg
             p-[calc(0.4vw+0.6rem)] gap-[calc(0.4vw+0.6rem)]`}>
             <span className={`text-[calc(0.4vw+0.6rem)] cursor-pointer text-center`}>{title}</span>
